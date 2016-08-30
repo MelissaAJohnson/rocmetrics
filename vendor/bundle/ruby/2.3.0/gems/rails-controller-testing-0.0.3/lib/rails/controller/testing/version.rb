964b250edecb6f7c66b4d54e7e0fca62502663b5
melissajstudent <missyj2007@gmail.com>
2016-08-29 19:34:57 -0500

    Cleaned up vendor/bundle mistake

--- vendor/bundle/ruby/2.3.0/gems/rails-controller-testing-0.0.3/lib/rails/controller/testing/version.rb
+++
@@ -1,7 +0,0 @@
-module Rails
-  module Controller
-    module Testing
-      VERSION = "0.0.3"
-    end
-  end
-end
--- vendor/bundle/ruby/2.3.0/gems/rails-controller-testing-0.0.3/test/dummy/app/controllers/assigns_controller.rb
+++
@@ -1,7 +0,0 @@
-class AssignsController < ActionController::Base
-  def test_assigns
-    @foo = "foo"
-    @foo_hash = { foo: :bar }
-    render nothing: true
-  end
-end
