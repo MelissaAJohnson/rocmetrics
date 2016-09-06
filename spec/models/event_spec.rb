31c4adc5d799e168a04812604973f5daa62b3b44
melissajstudent <missyj2007@gmail.com>
2016-09-05 22:14:43 -0500

    Event model spec added

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @event = FactoryGirl.create(:event)
+  end
+
+  it { is_expected.to belong_to(:application) }
+
+  describe "attributes" do
+    it "has name" do
+      expect(@event).to have_attributes(name: @event.name)
+    end
+  end
 end
